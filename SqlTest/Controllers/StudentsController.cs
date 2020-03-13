2612c9d8507146183f8bd0b8511c1cedcf0f897f
= <=>
2020-03-13 23:42:28 +0200

    Added A Coneole.WriteLine

--- SqlTest/Controllers/StudentsController.cs
+++ SqlTest/Controllers/StudentsController.cs
@@ -24,7 +24,9 @@ namespace SqlTest.Controllers
         // GET: api/Students
         public async Task<IEnumerable<Student>> GetStudents()
         {
+            Console.WriteLine("JJJ");
             return  await db.Students.ToListAsync();
+      
 
         }
 
